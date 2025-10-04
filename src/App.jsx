ff547cf5fb37e5ad2068d51c1fe92a4fceba36c3
Tarek <tarekg100@gmail.com>
2025-10-05 02:32:48 +0300

    prop list

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,54 @@ import Post from "./components/Post.jsx";
 import SideMenu from "./components/SideMenu.jsx";
 
 const showSideMenu = true;
+const posts = [
+  {
+    id: 1,
+    body: "أكاديمية ترميز مخصصة لتعليم البرمجة",
+    content: (
+      <div>
+        <h1>20</h1>
+        <h1>أكاديمية ترميز</h1>
+      </div>
+    ),
+  },
+  {
+    id: 2,
+    body: "this is the hello world article",
+    content: (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    body: "this is the body of post 3",
+    content: (
+      <div>
+        <h1>Post 3</h1>
+      </div>
+    ),
+  },
+];
 function App() {
+  const postList = posts.map((post) => {
+    return (
+      <Post key={post.id} body={post.body}>
+        {post.content}
+      </Post>
+    );
+  });
+
   return (
     <>
       <Header />
       {/*Posts & Side Container*/}
       <div style={{ width: "75%", margin: "auto", display: "flex" }}>
         {/*Posts Container*/}
-        <div style={{ width: "70%" }}>
-          <Post body="أكاديمية ترميز مخصصة لتعليم البرمجة">
-            <div>
-              <h1>20</h1>
-              <h1>أكاديمية ترميز</h1>
-            </div>
-          </Post>
-          <Post body="this is the hello world article">
-            <div>
-              <h1>Hello World</h1>
-            </div>
-          </Post>
-          <Post body="this is the body of post 3">
-            <div>
-              <h1>Post 3</h1>
-            </div>
-          </Post>
-        </div>
+        <div style={{ width: "70%" }}>{postList}</div>
         {/*Posts Container*/}
+
         {/*Side Container*/}
         <div style={{ width: "30%" }}>{sideMenuShow()}</div>
         {/*Side Container*/}
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,15 +1,32 @@
 import Button from "./menuButton.jsx";
 export default function SideMenu() {
+  const buttons = [
+    { id: 1, title: "مميز", content: <>&#10084;&#10084;</> },
+    { id: 2, title: "اكثر المقالات", content: <>&#9733;&#9733;</> },
+    {
+      id: 3,
+      title: "image",
+      content: (
+        <img
+          style={{ width: "100px", display: "block" }}
+          src="https://th.bing.com/th/id/R.34582bcd1f2313d02158df4e47123087?rik=LE9dsJaXkR%2bxJg&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2ftrees%2ftrees-07.jpg&ehk=fS7bCWA1J1cEgA2Z8Abx4idifVrJiRLc3Nip42qjm40%3d&risl=&pid=ImgRaw&r=0"
+        />
+      ),
+    },
+  ];
+  const buttonsList = buttons.map((button) => {
+    return (
+      <Button key={button.id} title={button.title}>
+        {button.content}
+      </Button>
+    );
+  });
   const Style = { border: "5px solid teal", margin: "25px" };
+
   return (
     <>
       <div className="SideMenu" style={Style}>
-        <Button title="مميز">&#10084;&#10084;</Button>
-        <Button title="اكثر المقالات">&#9733;&#9733;</Button>
-        <Button title="image">
-          <img style={{width:"100px", display:"block"}} src="https://th.bing.com/th/id/R.34582bcd1f2313d02158df4e47123087?rik=LE9dsJaXkR%2bxJg&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2ftrees%2ftrees-07.jpg&ehk=fS7bCWA1J1cEgA2Z8Abx4idifVrJiRLc3Nip42qjm40%3d&risl=&pid=ImgRaw&r=0"/>
-        </Button>
-        <Button></Button>
+        {buttonsList}
       </div>
     </>
   );
