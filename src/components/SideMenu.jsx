4b475d5f23913973a907cea20e6d78c6acf25c74
Tarek <tarekg100@gmail.com>
2025-10-04 19:41:47 +0300

    props

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -6,6 +6,9 @@ export default function SideMenu() {
       <div className="SideMenu" style={Style}>
         <Button title="مميز">&#10084;&#10084;</Button>
         <Button title="اكثر المقالات">&#9733;&#9733;</Button>
+        <Button title="image">
+          <img style={{width:"100px", display:"block"}} src="https://th.bing.com/th/id/R.34582bcd1f2313d02158df4e47123087?rik=LE9dsJaXkR%2bxJg&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2ftrees%2ftrees-07.jpg&ehk=fS7bCWA1J1cEgA2Z8Abx4idifVrJiRLc3Nip42qjm40%3d&risl=&pid=ImgRaw&r=0"/>
+        </Button>
         <Button></Button>
       </div>
     </>
--- src/components/menuButton.jsx
+++ src/components/menuButton.jsx
@@ -1,4 +1,8 @@
 import "./menuButton.css";
 export default function menuButton({ title = "no title", children = "" }) {
-  return <button className={"Menu-button"}>{title + " " + children}</button>;
+  return (
+    <button className={"Menu-button"}>
+      {title} {children}
+    </button>
+  );
 }
